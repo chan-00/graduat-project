12001b8a81ea40dcdc00a84dc89dab84352a0995
chan-00 <a82316@naver.com>
2023-01-02 20:17:39 +0900

    메인 페이지 Nav Bar 디자인 제작&Main GIF 영역 디자인

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+//react-routing
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+//user component import
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+//import App css
+import "./css/App.css";
+
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Header></Header>
+        <Routes>
+          <Route path="/" element={<Main></Main>}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Main.jsx
@@ -0,0 +1,22 @@
+//import Main css
+import "../css/Main.css";
+
+function Main() {
+    return (
+        <>
+            <div className="contentCenter" id="mainContentContainer">
+                <div>
+                상상하는 모든 것.<br/>
+                서일대학교에서.
+                </div>
+            </div>
+            <div className="contentCenter" id="mapContentContainer">
+                <div id="mapTitle">
+                    공간의 제약이 없는 프라이빗한 모임부터, 모두와 소통할 수 있는 광장까지.
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main;
