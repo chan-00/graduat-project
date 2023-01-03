c4ce98685e240d4c0fd065a717390e2972e82bd7
chan-00 <a82316@naver.com>
2023-01-03 13:15:43 +0900

    main 페이지 캐릭터 소개 영역 디자인 수정

--- src/Components/MainCharacterArea.jsx
+++ src/Components/MainCharacterArea.jsx
@@ -6,24 +6,26 @@ function MainCharacterArea() {
                 다양한 아바타를 골라 팀원들과 활동하세요!
             </div>
 
-            <div className="mapContainer" data-aos="fade-up" data-aos-duration="1200">
-                <h2>1. 남자1</h2>
-                <img src="https://media2.giphy.com/media/E5Hfk3kV0G5zcKPWy4/giphy.gif?cid=ecf05e47qjqmibqgahzkwf9og8phuofmfqartc3r748pnwum&rid=giphy.gif&ct=g" />
+            <div className="mainContentsContainer" data-aos="fade-up" data-aos-duration="1200">
+                <div>
+                    <h2>1. 남자1</h2>
+                    <img src="https://www.shutterstock.com/image-illustration/3d-illustration-smiling-happy-hipster-600w-1901479738.jpg" />
+                </div>
+                <div>
+                    <h2>2. 남자2</h2>
+                    <img src="https://www.shutterstock.com/image-illustration/3d-illustration-smiling-happy-hipster-600w-1901479738.jpg" />
+                </div>
             </div>
 
-            <div className="mapContainer" data-aos="fade-up" data-aos-duration="1200">
-                <h2>2. 남자2</h2>
-                <img src="https://media2.giphy.com/media/E5Hfk3kV0G5zcKPWy4/giphy.gif?cid=ecf05e47qjqmibqgahzkwf9og8phuofmfqartc3r748pnwum&rid=giphy.gif&ct=g" />
-            </div>
-
-            <div className="mapContainer" data-aos="fade-up" data-aos-duration="1200">
-                <h2>3. 여자1</h2>
-                <img src="https://media2.giphy.com/media/E5Hfk3kV0G5zcKPWy4/giphy.gif?cid=ecf05e47qjqmibqgahzkwf9og8phuofmfqartc3r748pnwum&rid=giphy.gif&ct=g" />
-            </div>
-
-            <div className="mapContainer" data-aos="fade-up" data-aos-duration="1200">
-                <h2>4. 여자2</h2>
-                <img src="https://media2.giphy.com/media/E5Hfk3kV0G5zcKPWy4/giphy.gif?cid=ecf05e47qjqmibqgahzkwf9og8phuofmfqartc3r748pnwum&rid=giphy.gif&ct=g" />
+            <div className="mainContentsContainer" data-aos="fade-up" data-aos-duration="1200">
+                <div>
+                    <h2>3. 여자1</h2>
+                    <img src="https://www.shutterstock.com/image-illustration/3d-illustration-smiling-happy-girl-600w-2053057919.jpg" />
+                </div>
+                <div>
+                    <h2>4. 여자2</h2>
+                    <img src="https://www.shutterstock.com/image-illustration/3d-illustration-smiling-happy-girl-600w-2053057919.jpg" />
+                </div>
             </div>
         </div>
     )
