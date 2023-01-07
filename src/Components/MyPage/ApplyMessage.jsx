b70f8d55b5b562991c32339be8195c01304a330e
chan-00 <a82316@naver.com>
2023-01-07 20:17:33 +0900

    MyPage에서 Apply History 영역 디자인 작업

--- src/Components/MyPage/ApplyMessage.jsx
+++ src/Components/MyPage/ApplyMessage.jsx
@@ -1,7 +1,7 @@
 //import css
 import "../../css/MyPageCss/ApplyMessage.css";
 //import react bootstrap icons
-import { PersonCircle } from "react-bootstrap-icons";
+import { PersonCircle, X, EmojiFrown } from "react-bootstrap-icons";
 //import react bootstrap
 import { Button } from "react-bootstrap"
 
@@ -11,9 +11,33 @@ function ApplyMessage() {
             <h3>Apply History</h3>
             <h3>Applied History</h3>
             <div id="applyMessageContainer">
-                
+                <div className="applyContents">
+                    JavaScript 스터디 신청
+                </div>
+                <div className="applyContents">
+                    Unity 스터디 신청
+                </div>
+                <div className="applyContents">
+                    Photon 스터디 신청
+                </div>
+                <div className="applyContents">
+                    Django 스터디 신청
+                </div>
+                <div className="applyContents">
+                    Photon 스터디 신청
+                </div>
+                <div className="applyContents">
+                    Django 스터디 신청
+                </div>
+                <div className="applyContents">
+                    Photon 스터디 신청
+                </div>
+                <div className="applyContents">
+                    Django 스터디 신청
+                </div>
             </div>
             <div id="appliedMessageContainer">
+                {/*
                 <div className="appliedContents">
                     <PersonCircle></PersonCircle>
                     <div className="bubble">
@@ -45,6 +69,10 @@ function ApplyMessage() {
                         <Button variant="outline-danger" className="appliedBtn">거절</Button>
                         <Button variant="outline-primary" className="appliedBtn">수락</Button>
                     </div>
+                </div>
+                */}
+                <div className="noneAppliedIcon">
+                    <p>None history....</p>
                 </div>
             </div>
         </div>
