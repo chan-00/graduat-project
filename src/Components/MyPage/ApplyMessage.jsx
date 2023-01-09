0b746e0f650d13ba11ce8b46a2cb1a52d355a0a4
chan-00 <a82316@naver.com>
2023-01-09 19:43:13 +0900

    MyPage에서 Applied History 디자인 변경

--- src/Components/MyPage/ApplyMessage.jsx
+++ src/Components/MyPage/ApplyMessage.jsx
@@ -1,73 +1,112 @@
 //import css
 import "../../css/MyPageCss/ApplyMessage.css";
 //import react bootstrap icons
-import { PersonCircle, X, EmojiFrown } from "react-bootstrap-icons";
+import { PersonCircle } from "react-bootstrap-icons";
 //import react bootstrap
-import { Button } from "react-bootstrap"
+import { Button, Card } from "react-bootstrap"
 
 function ApplyMessage() {
     return (
-        <div id="applyMessageAllContainer">
-            <h3>Apply History</h3>
+        <>
             <h3>Applied History</h3>
-            <div id="applyMessageContainer">
-                <div className="applyContents">
-                    JavaScript 스터디 신청
-                </div>
-                <div className="applyContents">
-                    Unity 스터디 신청
-                </div>
-                <div className="applyContents">
-                    Photon 스터디 신청
-                </div>
-                <div className="applyContents">
-                    Django 스터디 신청
-                </div>
-                <div className="applyContents">
-                    Photon 스터디 신청
-                </div>
-                <div className="applyContents">
-                    Django 스터디 신청
-                </div>
-                <div className="applyContents">
-                    Photon 스터디 신청
-                </div>
-                <div className="applyContents">
-                    Django 스터디 신청
-                </div>
-            </div>
             <div id="appliedMessageContainer">
-                <div className="appliedContents">
-                    <PersonCircle></PersonCircle>
-                    <div className="bubble">
-                        ChanYoung<span>(JavaScript 스터디)</span>
-                        <Button variant="outline-danger" className="appliedBtn">거절</Button>
-                        <Button variant="outline-primary" className="appliedBtn">수락</Button>
-                    </div>
+                <div className="cardContainer">
+                    <Card>
+                    <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" />
+                        <Card.Body>
+                            <Card.Title>ChanYoung</Card.Title>
+                            <Card.Text>
+                                <p>안녕하세요, 저는 JavaScript 개발자입니다.</p>
+                                <span className="userEmail">a37975387@gmail.com</span>
+                            </Card.Text>
+                            <button className="outlintPrimary">승인</button>
+                            <button className="outlintDanger">거절</button>
+                        </Card.Body>
+                    </Card>
                 </div>
-                <div className="appliedContents">
-                    <PersonCircle></PersonCircle>
-                    <div className="bubble">
-                        ParkJun<span>(Photon 스터디)</span>
-                        <Button variant="outline-danger" className="appliedBtn">거절</Button>
-                        <Button variant="outline-primary" className="appliedBtn">수락</Button>
-                    </div>
+                <div className="cardContainer">
+                    <Card>
+                    <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" />
+                        <Card.Body>
+                            <Card.Title>ChanYoung</Card.Title>
+                            <Card.Text>
+                                <p>안녕하세요, 저는 JavaScript 개발자입니다.</p>
+                                <span className="userEmail">a37975387@gmail.com</span>
+                            </Card.Text>
+                            <button className="outlintPrimary">승인</button>
+                            <button className="outlintDanger">거절</button>
+                        </Card.Body>
+                    </Card>
                 </div>
-                <div className="appliedContents">
-                    <PersonCircle></PersonCircle>
-                    <div className="bubble">
-                        Agujun<span>(Unity 스터디)</span>
-                        <Button variant="outline-danger" className="appliedBtn">거절</Button>
-                        <Button variant="outline-primary" className="appliedBtn">수락</Button>
-                    </div>
+                <div className="cardContainer">
+                    <Card>
+                    <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" />
+                        <Card.Body>
+                            <Card.Title>ChanYoung</Card.Title>
+                            <Card.Text>
+                                <p>안녕하세요, 저는 JavaScript 개발자입니다.</p>
+                                <span className="userEmail">a37975387@gmail.com</span>
+                            </Card.Text>
+                            <button className="outlintPrimary">승인</button>
+                            <button className="outlintDanger">거절</button>
+                        </Card.Body>
+                    </Card>
                 </div>
-                <div className="appliedContents">
-                    <PersonCircle></PersonCircle>
-                    <div className="bubble">
-                        Maple<span>(Django 스터디)</span>
-                        <Button variant="outline-danger" className="appliedBtn">거절</Button>
-                        <Button variant="outline-primary" className="appliedBtn">수락</Button>
-                    </div>
+                <div className="cardContainer">
+                    <Card>
+                    <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" />
+                        <Card.Body>
+                            <Card.Title>ChanYoung</Card.Title>
+                            <Card.Text>
+                                <p>안녕하세요, 저는 JavaScript 개발자입니다.</p>
+                                <span className="userEmail">a37975387@gmail.com</span>
+                            </Card.Text>
+                            <button className="outlintPrimary">승인</button>
+                            <button className="outlintDanger">거절</button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="cardContainer">
+                    <Card>
+                    <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" />
+                        <Card.Body>
+                            <Card.Title>ChanYoung</Card.Title>
+                            <Card.Text>
+                                <p>안녕하세요, 저는 JavaScript 개발자입니다.</p>
+                                <span className="userEmail">a37975387@gmail.com</span>
+                            </Card.Text>
+                            <button className="outlintPrimary">승인</button>
+                            <button className="outlintDanger">거절</button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="cardContainer">
+                    <Card>
+                    <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" />
+                        <Card.Body>
+                            <Card.Title>ChanYoung</Card.Title>
+                            <Card.Text>
+                                <p>안녕하세요, 저는 JavaScript 개발자입니다.</p>
+                                <span className="userEmail">a37975387@gmail.com</span>
+                            </Card.Text>
+                            <button className="outlintPrimary">승인</button>
+                            <button className="outlintDanger">거절</button>
+                        </Card.Body>
+                    </Card>
+                </div>
+                <div className="cardContainer">
+                    <Card>
+                    <Card.Img variant="top" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" />
+                        <Card.Body>
+                            <Card.Title>ChanYoung</Card.Title>
+                            <Card.Text>
+                                <p>안녕하세요, 저는 JavaScript 개발자입니다.</p>
+                                <span className="userEmail">a37975387@gmail.com</span>
+                            </Card.Text>
+                            <button className="outlintPrimary">승인</button>
+                            <button className="outlintDanger">거절</button>
+                        </Card.Body>
+                    </Card>
                 </div>
                 {/*
                 <div className="noneAppliedIcon">
@@ -75,7 +114,7 @@ function ApplyMessage() {
                 </div>
                 */}
             </div>
-        </div>
+        </>
     )
 }
 
