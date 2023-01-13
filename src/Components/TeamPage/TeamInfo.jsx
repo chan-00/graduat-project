0fcd5788214f604cb03370f75a75ee89db80b6b7
chan-00 <a82316@naver.com>
2023-01-13 12:33:20 +0900

    Team Main Page에 Loading Spinner 적용

--- src/Components/TeamPage/TeamInfo.jsx
+++ src/Components/TeamPage/TeamInfo.jsx
@@ -1,10 +1,19 @@
 //import css
 import "../../css/TeamPageCss/TeamInfo.css";
+//import react hooks
+import { useState } from "react";
+//import react sidebar
+import SidebarMenu from "react-bootstrap-sidebar-menu";
 
 function TeamInfo() {
+    //현재 팀 상세 페이지에 들어온 계정이 해당 팀에 속해 있는지, 속해 있다면 팀장인지에 대한 여부 값이 담길 useState 변수
+    const [ teamBelong, setTeamBelong ] = useState("");
+    //현재 사이드바 옆에 어떤 내용을 출력할지에 대한 내용이 담긴 useState 변수
+    const [ selectedMenu, setSelectedMenu ] = useState("Main");
+
     return (
         <div>
-            안녕~
+            
         </div>
     )
 }
--- src/Components/TeamPage/TeamMain.jsx
+++ src/Components/TeamPage/TeamMain.jsx
@@ -1,22 +1,40 @@
 //import react bootstrap
 import Table from 'react-bootstrap/Table';
 import Pagination from 'react-bootstrap/Pagination';
+import Spinner from 'react-bootstrap/Spinner';
 //import css
 import "../../css/TeamPageCss/TeamMain.css";
 //import react bootstrap icons
 import { PersonCircle } from "react-bootstrap-icons"
 //import react hooks
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+//import functions
+import functionGetTeamInfoList from '../../Functions/FunctionTeam/functionGetTeamInfoList';
 
 
 //자신이 갖고 있는 팀의 리스트를 볼 수 있는 팀 메인 페이지 component
 function TeamMain() {
+
+    //전체 팀 리스트 배열 값을 담을 useState 변수
+    const [ userTeamInfoList, setUserTeamInfoList ] = useState([]);
+    //현재 페이지에 보여질 팀 리스트를 담을 배열 useState 변수(pagination 관련)
+    const [ currentTeamInfo, setCurrentTeamInfo ] = useState([]);
+    //현재 클릭되어 있는 pagination 번호 값을 담을 useState 변수
+    const [ currentPageNum, setCurrentPageNum ] = useState(1);
+    //로딩 화면을 표시하기 위한 status 변수
+    const [ loadingStatus, setLoadingStatus ] = useState(false);
+
+    //처음 페이지 렌더링 시 팀 리스트 값을 받아오기 위한 useEffect 함수
+    useEffect(() => {
+        functionGetTeamInfoList(setUserTeamInfoList, window.sessionStorage.id, setLoadingStatus);
+    }, []);
 
     //pagination에서 마우스로 클릭 시 해당 버튼에 active 효과 부여하기 위한 onclick 함수
     const handlePaginationBtnOnClick = (e) => {
         
     }
     //pagination 예시를 위한 items 배열 생성
+    /*
     let items = [];
     for (let number = 1; number <= 5; number++) {
         items.push(
@@ -25,98 +43,115 @@ function TeamMain() {
             </Pagination.Item>,
         );
     }
+    */
     
-    return (
-        <div id="teamMainAllContainer">
-            <Table id='teamMainContentsContainer'>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Team Name</th>
-                        <th>Start Date</th>
-                        <th>Member</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>JavaScript 스터디</td>
-                        <td>2019/03/02</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Unity 스터디</td>
-                        <td>2020/03/04</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Django 스터디</td>
-                        <td>2021/04/05</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>JavaScript 스터디</td>
-                        <td>2019/03/02</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>Unity 스터디</td>
-                        <td>2020/03/04</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>Django 스터디</td>
-                        <td>2021/04/05</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>7</td>
-                        <td>JavaScript 스터디</td>
-                        <td>2019/03/02</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>8</td>
-                        <td>Unity 스터디</td>
-                        <td>2020/03/04</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>9</td>
-                        <td>Django 스터디</td>
-                        <td>2021/04/05</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>10</td>
-                        <td>JavaScript 스터디</td>
-                        <td>2019/03/02</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>11</td>
-                        <td>Unity 스터디</td>
-                        <td>2020/03/04</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                    <tr>
-                        <td>12</td>
-                        <td>Django 스터디</td>
-                        <td>2021/04/05</td>
-                        <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
-                    </tr>
-                </tbody>
-            </Table>
-            <div id="teamMainPaginationContainer">
-                <Pagination id='paginationContainer'>{items}</Pagination>
+    if(userTeamInfoList.length !== 0 && loadingStatus) {
+        return (
+            <div id="teamMainAllContainer">
+                <Table id='teamMainContentsContainer'>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Team Name</th>
+                            <th>Start Date</th>
+                            <th>Member</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>JavaScript 스터디</td>
+                            <td>2019/03/02</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Unity 스터디</td>
+                            <td>2020/03/04</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Django 스터디</td>
+                            <td>2021/04/05</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>4</td>
+                            <td>JavaScript 스터디</td>
+                            <td>2019/03/02</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>5</td>
+                            <td>Unity 스터디</td>
+                            <td>2020/03/04</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>6</td>
+                            <td>Django 스터디</td>
+                            <td>2021/04/05</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>7</td>
+                            <td>JavaScript 스터디</td>
+                            <td>2019/03/02</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>8</td>
+                            <td>Unity 스터디</td>
+                            <td>2020/03/04</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>9</td>
+                            <td>Django 스터디</td>
+                            <td>2021/04/05</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>10</td>
+                            <td>JavaScript 스터디</td>
+                            <td>2019/03/02</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>11</td>
+                            <td>Unity 스터디</td>
+                            <td>2020/03/04</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                        <tr>
+                            <td>12</td>
+                            <td>Django 스터디</td>
+                            <td>2021/04/05</td>
+                            <td><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle><PersonCircle></PersonCircle></td>
+                        </tr>
+                    </tbody>
+                </Table>
+                <div id="teamMainPaginationContainer">
+                    <Pagination id='paginationContainer'>1</Pagination>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    else if(userTeamInfoList.length === 0 && loadingStatus) {
+        return (
+            <div id="teamMainAllContainer">
+                속해 있는 팀이 없습니다.
+            </div>
+        )
+    }
+    else if(!loadingStatus) {
+        return (
+            <div id="teamMainAllContainer" style={{textAlign:"center"}}>
+                <Spinner animation="border" />
+            </div>
+        )
+    }
 }
 
 export default TeamMain;
